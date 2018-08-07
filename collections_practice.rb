6eb182165e63c5e084c0a623ab06e8bd2439e19c
Arielle N Ramirez <ariellenramirez@gmail.com>
2018-08-07 13:05:00 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,41 @@
+def sort_array_asc(integers)
+  integers.sort
+  
+end
 
+
+def sort_array_desc(integers)
+  integers.sort{|x,y| y <=> x}
+  
+end
+
+def sort_array_char_count(animals)
+  animals.sort_by {|x| x.length}
+  
+end
+
+def swap_elements(names)
+  names[0],names[1],names[2]= names[0],names[2],names[1]
+  
+end
+
+def reverse_array(arr)
+  arr.reverse
+end
+
+def kesha_maker(dollar)
+  
+end
+
+
+def sum_array(array)
+[11,4,7,8,9,100,134].inject{|memo, num| memo += num; memo}
+end
+
+def find_a(dictionary)
+  dictionary.select { |word| word.start_with?('a')}
+end
+
+def add_s(element, index)
+
+end
